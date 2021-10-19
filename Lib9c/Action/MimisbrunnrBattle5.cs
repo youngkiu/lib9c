c001fa4eb2ff4f6415f515c2543469f592484407
hskim881028 <andykim881028@gmail.com>
2021-10-19 15:05:22 +0900

    Version up MimisbrunnrBattle action

--- Lib9c/Action/MimisbrunnrBattle5.cs
+++ Lib9c/Action/MimisbrunnrBattle5.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
@@ -16,8 +16,9 @@ using static Lib9c.SerializeKeys;
 namespace Nekoyume.Action
 {
     [Serializable]
-    [ActionType("mimisbrunnr_battle6")]
-    public class MimisbrunnrBattle : GameAction
+    [ActionObsolete(BlockChain.Policy.BlockPolicySource.V100082ObsoleteIndex)]
+    [ActionType("mimisbrunnr_battle5")]
+    public class MimisbrunnrBattle5 : GameAction
     {
         public List<Guid> costumes;
         public List<Guid> equipments;
@@ -67,6 +68,8 @@ namespace Nekoyume.Action
                     .SetState(questListAddress, MarkChanged);
                 return states.SetState(ctx.Signer, MarkChanged);
             }
+
+            CheckObsolete(BlockChain.Policy.BlockPolicySource.V100082ObsoleteIndex, context);
 
             var addressesHex = GetSignerAndOtherAddressesHex(context, avatarAddress);
 
